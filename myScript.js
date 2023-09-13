1508d521680a9d36892a595b71a9c3d91cd21aef
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-09-12 19:37:45 -0500

    have functioning resizeable grid

--- myScript.js
+++ myScript.js
@@ -1,11 +1,21 @@
-function createGrid(){
-  const body = document.body;
+const body = document.body;
 
-  for(let i=0; i<4; i++){
+function removeElement(id) {
+  let elem = document.getElementById(id);
+  return elem.parentNode.removeChild(elem);
+}
+
+function createGrid(numSquares){
+
+  let grid = document.createElement('div');
+  grid.className = "grid";
+  grid.id = "gridId"
+
+  for(let i=0; i<numSquares; i++){
     let row = document.createElement('div');
     row.className = "row";
 
-    for(let j=0; j<4; j++){
+    for(let j=0; j<numSquares; j++){
       let square = document.createElement('div');
       square.className = "square";
       square.addEventListener('mouseover', () => {
@@ -13,12 +23,34 @@ function createGrid(){
       });
       row.appendChild(square);
     }
-    body.appendChild(row);
+    grid.appendChild(row);
   }
 
-  
+  body.appendChild(grid);
+
+  return grid;
+}
+
+function main(){
+  let numSquares = 4;
+  let grid;
+
+  // setup grid size button
+  let gridSizeButton = document.createElement('button');
+  gridSizeButton.className = "grid-size-button";
+  gridSizeButton.addEventListener('click', () => {
+    numSquares = prompt("How many squares per side do you want for the grid? Enter a number.");
+    // remove old grid
+    if(document.getElementsByClassName("grid")){
+      grid.remove();
+    }
+    createGrid(numSquares);
+  }); 
+  body.appendChild(gridSizeButton);
+
+  grid = createGrid(numSquares);
 
   return;
 }
 
-createGrid();
+main();
